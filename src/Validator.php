caa07d4a51500f9a397f0747b07eb441358ee9ea
drew-acheson <dacheson@pdga.com>
2023-03-28 15:25:07 -0700

    updated comments as doc tags

--- src/Validator.php
+++ src/Validator.php
@@ -2,15 +2,24 @@
 
 /**
  * All concrete validators should implement this interface.
- *
- * function validate(mixed $val): boolean This returns true if $val is valid; else false.
- *
- * function getErrorMessage(string $propName): string A function that returns an error
- * message if validation fails. $propName is the name of the field that failed validation.
- * For example, 'The "email" field must be a valid string.' might be an appropriate error
- * message for a StringValidator.
  */
 interface Validator {
+
+    /**
+     * This returns true if $val is valid; else false.
+     *
+     * @param mixed $val The value to validate.
+     * @return boolean Returns true if $val is valid; else false.
+     */
     public function validate(mixed $val): boolean;
+
+    /**
+     * Called to get an error message on validation failure.
+     *
+     * @param string $propName The name of the field that failed validation.
+     * @return an error message as a string.  For example, 'The "email" field
+     * must be a valid string.' might be an appropriate error message for
+     * a StringValidator.
+     */
     public function getErrorMessage(string $propName): string;
 }
