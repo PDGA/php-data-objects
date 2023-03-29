beb529e913a77db4a5db76a027ebdecaeed9997f
drew-acheson <dacheson@pdga.com>
2023-03-29 14:26:56 -0700

    added doc block comments to functions, removed empty constructor, updated error message to include the fact that the array should be zero-indexed

--- src/SequentialArrayValidator.php
+++ src/SequentialArrayValidator.php
@@ -6,10 +6,14 @@ use PDGA\DataObjects\Validator;
 
 class SequentialArrayValidator implements Validator
 {
-    public function __construct() {
-
-    }
-
+    /**
+     * Validates that the passed in value is null, or is undefined,
+     * or is an array with sequential numeric keys which start at zero.
+     *
+     * @param mixed $val The value to validate.
+     * @return bool Returns true if the passed in value is null, undefined, or
+     * an array with sequential numeric keys that start at zero.
+     */
     public function validate(mixed $val): bool
     {
         if (is_null($val) || !isset($val))
@@ -37,8 +41,16 @@ class SequentialArrayValidator implements Validator
         return true;
     }
 
+    /**
+     * Returns an error indicating that the passed in property name is supposed to be a
+     * sequential (non-associative) zero-indexed array.
+     *
+     * @param string $propName The name of the property.
+     * @return string Returns an error string which includes the name of the property and
+     * the valid type the property should be.
+     */
     public function getErrorMessage(string $propName): string
     {
-        return "The $propName field must be a sequential (non-associative) array.";
+        return "The $propName field must be a sequential (non-associative) zero-indexed array.";
     }
 }
