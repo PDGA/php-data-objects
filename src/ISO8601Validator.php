0509fff050a062aa1c709c49d6d551d0acbf5eb3
drew-acheson <dacheson@pdga.com>
2023-03-31 09:47:38 -0700

    removed some capture groups and a set of parens

--- src/ISO8601Validator.php
+++ src/ISO8601Validator.php
@@ -29,9 +29,9 @@ class ISO8601Validator implements Validator
         }
 
         if (!preg_match('/^'.
-            '(\d{4}-\d{2}-\d{2})(T'. // YYYY-MM-DDT ex: 2014-01-01T
+            '(\d{4}-\d{2}-\d{2})(?:T'. // YYYY-MM-DDT ex: 2014-01-01T
             '\d{2}:\d{2}:\d{2}'.  // HH:MM:SS  ex: 17:00:00
-            '(Z|((-|\+)\d{2}:\d{2})))?'.  // Z or +01:00 or -01:00
+            '(?:Z|(?:[-|\+]\d{2}:\d{2})))?'.  // Z or +01:00 or -01:00
             '$/', $val, $matches))
         {
             return false;
