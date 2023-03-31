f6f1a7f829249ffd7bc420e1bd45ee2546aa210d
drew-acheson <dacheson@pdga.com>
2023-03-31 12:49:34 -0700

    tests for new not null validator

---
+++ src/NotNullValidatorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PDGA\DataObjects\NotNullValidator;
+
+final class NotNullValidatorTest extends TestCase
+{
+    private $not_null_validator;
+
+    protected function setUp(): void
+    {
+        $this->not_null_validator = new NotNullValidator();
+    }
+
+    /**
+     * Make sure the error message returned is as expected.
+     */
+    public function testGetErrorMessage(): void
+    {
+        $expected_value = "The variable field must not be null.";
+        $result = $this->not_null_validator->getErrorMessage("variable");
+
+        $this->assertSame($expected_value, $result);
+    }
+
+    /**
+     * Nulls should result in false.
+     */
+    public function testNullPassedIn(): void
+    {
+        $expected_value = false;
+        $result = $this->not_null_validator->validate(null);
+
+        $this->assertSame($expected_value, $result);
+    }
+
+    /**
+     * A defined value should result in true.
+     */
+    public function testDefinedValues(): void
+    {
+        $valid_values = [
+            'this is a string',
+            123456,
+            true,
+            0,
+            false,
+            '',
+            ' ',
+        ];
+
+        foreach ($valid_values as $value)
+        {
+            $this->assertTrue($this->not_null_validator->validate($value));
+        }
+    }
+}
