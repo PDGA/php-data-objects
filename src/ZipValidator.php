7929bc753f45aea81be1a1c1d8a415c2299f59bd
drew-acheson <dacheson@pdga.com>
2023-04-03 08:11:13 -0700

    updated comments

--- src/ZipValidator.php
+++ src/ZipValidator.php
@@ -10,7 +10,7 @@ class ZipValidator implements Validator
      * Validates that the passed in value is null or at most 15 characters long.
      *
      * @param mixed $val The value to validate.
-     * @return bool Returns true if the passed in value is not null.
+     * @return bool Returns true if the passed in value is not null or at most 15 characters long.
      */
     public function validate(mixed $val): bool
     {
@@ -34,7 +34,7 @@ class ZipValidator implements Validator
 
     /**
      * Returns an error indicating that the passed in property name is supposed to be
-     * no longer than 15 characters long.
+     * no longer than 15 characters long and not be an array.
      *
      * @param string $propName The name of the property.
      * @return string Returns an error string which includes the name of the property and
@@ -42,6 +42,6 @@ class ZipValidator implements Validator
      */
     public function getErrorMessage(string $propName): string
     {
-        return "The $propName field must be no longer than 15 characters.";
+        return "The $propName field must not be an array and must be no longer than 15 characters.";
     }
 }
