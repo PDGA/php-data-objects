08e26c8bab95cb0c709800ed07097f4c678afd25
drew-acheson <dacheson@pdga.com>
2023-04-04 09:22:13 -0700

    added doc block comments to table class

--- src/Attributes/Table.php
+++ src/Attributes/Table.php
@@ -9,11 +9,21 @@ class Table
 {
     private $table_name;
 
+    /**
+     * Constructor for the Table attribute.
+     *
+     * @param string $name - The name of the table.
+     */
     public function __construct(string $name)
     {
         $this->table_name = $name;
     }
 
+    /**
+     * Returns the name of the table.
+     *
+     * @return string Returns the name of the table.
+     */
     public function getName(): string
     {
         return $this->table_name;
