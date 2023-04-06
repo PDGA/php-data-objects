378c13ac0a39db86064bfdf16342336776301d2c
drew-acheson <dacheson@pdga.com>
2023-04-06 08:37:29 -0700

    new validation enforcer and tests

---
+++ src/Enforcers/ValidationEnforcer.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace PDGA\DataObjects\Enforcers;
+
+use \ReflectionClass;
+
+use PDGA\DataObjects\Validators\BoolValidator;
+use PDGA\DataObjects\Validators\DateValidator;
+use PDGA\DataObjects\Validators\IntValidator;
+use PDGA\DataObjects\Validators\NotNullValidator;
+use PDGA\DataObjects\Validators\StringValidator;
+use PDGA\Exception\ValidationListException;
+
+class ValidationEnforcer
+{
+    /**
+     * Validates the properties of an object against a class definition.
+     *
+     * @throws ValidationListException - Throws a ValidationListException which
+     * includes a cumulative list of errors encountered during validation for each
+     * property on the object.
+     */
+    public function validate(mixed $object, string $className): void
+    {
+
+        $validators = [
+            "int"      => new IntValidator(),
+            "string"   => new StringValidator(),
+            "bool"     => new BoolValidator(),
+            "DateTime" => new DateValidator(),
+            "null"     => new NotNullValidator(),
+        ];
+
+        $arr = is_array($object) ? $object : (array) $object;
+        $validationErrors = new ValidationListException();
+
+        $ref   = new ReflectionClass($className);
+        $props = $ref->getProperties();
+
+        //For each property defined on the class.
+        foreach($props as $prop)
+        {
+            $propName    = $prop->getName();
+            $propType    = $prop->getType()->getName();
+            $propAttrs   = $prop->getAttributes();
+            $propCanNull = $prop->getType()->allowsNull();
+
+            //If the class property is passed in with the object.
+            if ($this->propIsDefined($arr, $propName))
+            {
+                //If the property is not null and a validator exists for that type make sure the type is correct.
+                $validator = array_key_exists($propType, $validators) ? $validators[$propType] : null;
+                if ($this->propIsNotNull($arr, $propName) && !is_null($validator))
+                {
+                    if (!$validator->validate($arr[$propName]))
+                    {
+                        $validationErrors->addError($validator->getErrorMessage($propName), $propName);
+                    }
+                }
+                //If the property is null make sure it is allowed to be null.
+                else if (!$propCanNull)
+                {
+                    $validationErrors->addError($validators["null"]->getErrorMessage($propName), $propName);
+                    $validationErrors->addError($validator->getErrorMessage($propName), $propName);
+                }
+
+                //If the property has any attributes make sure they are enforced.
+                foreach($propAttrs as $attr)
+                {
+                    $validatorName = $attr->getName();
+                    $attrValidator = new $validatorName(...$attr->getArguments());
+
+                    if (!$attrValidator->validate($arr[$propName]))
+                    {
+                        $validationErrors->addError($attrValidator->getErrorMessage($propName), $propName);
+                    }
+                }
+            }
+        }
+
+        if (count($validationErrors->getErrors()))
+        {
+            throw $validationErrors;
+        }
+
+    }
+
+    /**
+     * Returns true if the property is defined on the object.
+     *
+     * @param mixed $object - Either an object or an array on which to determine
+     * if the property is defined.
+     * @param string $property - The property being looked for.
+     * @return bool Returns true if the property is defined on the object.
+     */
+    public function propIsDefined(mixed $object, string $property): bool
+    {
+        $arr = is_array($object) ? $object : (array) $object;
+
+        return array_key_exists($property, $arr);
+    }
+
+    /**
+     * Returns true if the property is defined as null on the object.
+     *
+     * @param mixed $object - Either an object or an array on which to determine
+     * if the property is defined as null.
+     * @param string $property - The property being looked at.
+     * @return bool Returns true if the property is defined as null on the object.
+     */
+    public function propIsNull(mixed $object, string $property): bool
+    {
+        $arr = is_array($object) ? $object : (array) $object;
+
+        return $this->propIsDefined($arr, $property) && is_null($arr["$property"]);
+    }
+
+    /**
+     * Returns the inverse of the propIsDefined function.  See above.
+     *
+     * @param mixed $object - Either an object or an array on which to determine
+     * if the property is not defined.
+     * @param string $property - The property being looked for.
+     * @return bool Returns true if the property is not defined on the object.
+     */
+    public function propIsUndefined(mixed $object, string $property): bool
+    {
+        return !$this->propIsDefined($object, $property);
+    }
+
+    /**
+     * Returns true if the property is not defined as null on the object.
+     *
+     * @param mixed $object - Either an object or an array on which to determine
+     * if the property is not defined as null.
+     * @param string $property - The property being looked for.
+     * @return bool Returns true if the property is not defined as null on the object.
+     */
+    public function propIsNotNull(mixed $object, string $property): bool
+    {
+        $arr = is_array($object) ? $object : (array) $object;
+
+        return $this->propIsDefined($arr, $property) && !is_null($arr["$property"]);
+    }
+}
