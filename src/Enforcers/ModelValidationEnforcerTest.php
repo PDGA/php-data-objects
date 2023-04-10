ecc4b8979634fbc90c78747fd0bc4d95a1888af0
drew-acheson <dacheson@pdga.com>
2023-04-10 11:52:07 -0700

    tests for new model validation enforcer class

---
+++ src/Enforcers/ModelValidationEnforcerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PDGA\DataObjects\Attributes\Column;
+use PDGA\DataObjects\Enforcers\ModelValidationEnforcer;
+use PDGA\DataObjects\Validators\EmailValidator;
+use PDGA\DataObjects\Validators\MaxLengthValidator;
+
+class NamedPerson
+{
+    #[Column(
+        name: 'email',
+        maxLength: 30
+    )]
+    #[MaxLengthValidator(15), EmailValidator]
+    public ?string $email;
+    public int $id;
+    #[Column(
+        name: 'name',
+        maxLength: 20
+    )]
+    #[MaxLengthValidator(15)]
+    public string $name;
+}
+
+class ModelValidationEnforcerTest extends TestCase
+{
+    private $enforcer;
+
+    protected function setUp(): void
+    {
+        $this->enforcer = new ModelValidationEnforcer();
+    }
+
+    public function testMetadataReturnsCorrectNumberOfValidators(): void
+    {
+        $result = $this->enforcer->getValidationMetadata(NamedPerson::class);
+
+        //id validators include int and not null
+        $this->assertEquals(count($result['id']['validators']), 2);
+        //email validators include string, max length, max length (from the column) and email
+        $this->assertEquals(count($result['email']['validators']), 4);
+        //name validators include string, max length, max length (from the column), and not null
+        $this->assertEquals(count($result['email']['validators']), 4);
+    }
+
+}
