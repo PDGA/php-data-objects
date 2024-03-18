4226d71a3677b0a898e199be4c1036df3a21eba4
Drew Acheson <dacheson@pdga.com>
2024-03-18 12:43:45 -0700

    new interface for database models

---
+++ src/Interfaces/IDatabaseModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PDGA\DataObjects\Interfaces;
+
+interface IDatabaseModel {
+    /**
+     * Get the attributes of the database model.
+     *
+     * @return array The attributes of the database model.
+     */
+    public function getAttributes(): array;
+
+    /**
+     * Get the relations of the database model.
+     *
+     * @return array The relations of the database model.
+     */
+    public function getRelations(): array;
+}
